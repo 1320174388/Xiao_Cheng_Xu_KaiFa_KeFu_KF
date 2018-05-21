c49756c59de9efd54803b581309bc50627ae0813
chengtao <1430125429@qq.com>
2018-05-21 17:07:26 +0800

    20180521_170607_first

---
+++ client/pages/home/index/index.js
@@ -0,0 +1,97 @@
+// pages/home/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      shuiQi:[
+        "../images/images-index/s1.jpg",
+        "../images/images-index/s2.jpg",
+        "../images/images-index/s3.jpg",
+        "../images/images-index/s4.jpg",
+        "../images/images-index/s5.jpg",
+        "../images/images-index/s6.jpg",
+        "../images/images-index/s7.jpg",
+        "../images/images-index/s8.jpg",
+        "../images/images-index/s9.jpg",
+        "../images/images-index/s10.jpg",
+        "../images/images-index/s11.jpg",
+        "../images/images-index/s12.jpg",
+        "../images/images-index/s13.jpg",
+        "../images/images-index/s14.jpg",
+        "../images/images-index/s15.jpg",
+        "../images/images-index/s16.jpg",
+        "../images/images-index/s17.jpg",
+        "../images/images-index/s18.jpg"
+      ],
+      "yuanMu":[
+        "../images/images-index/y1.jpg",
+        "../images/images-index/y2.jpg",
+        "../images/images-index/y3.jpg",
+        "../images/images-index/y4.jpg",
+        "../images/images-index/y5.jpg",
+        "../images/images-index/y6.jpg",
+        "../images/images-index/y7.jpg",
+        "../images/images-index/y8.jpg",
+        "../images/images-index/y9.jpg",
+        "../images/images-index/y10.jpg"
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
